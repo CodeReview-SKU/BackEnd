4e854a3cafd2ad8f757c814919044576636bdfb6
HwanheeMoon <yuiop0218@naver.com>
2024-01-18 18:25:15 +0900

    멤버 테이블 서비스, 레포지토리, 검색 오류 예외 처리 구현

--- src/main/java/com/Project/BackEnd/Member/Repository/MemberRepository.java
+++ src/main/java/com/Project/BackEnd/Member/Repository/MemberRepository.java
@@ -1,2 +1,14 @@
-package com.Project.BackEnd.Member.Repository;public class MemberRepository {
+package com.Project.BackEnd.Member.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Project.BackEnd.Member.Entity.Member;
+import java.util.Optional;
+
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
 }
