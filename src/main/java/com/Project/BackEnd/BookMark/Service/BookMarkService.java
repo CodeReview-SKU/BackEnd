623e0bfb48fffafb771d6c9cb39ee45a540f69ec
HwanheeMoon <yuiop0218@naver.com>
2024-01-18 18:20:30 +0900

    북마크 레포지토리, 서비스 구현

--- src/main/java/com/Project/BackEnd/BookMark/Service/BookMarkService.java
+++ src/main/java/com/Project/BackEnd/BookMark/Service/BookMarkService.java
@@ -1,2 +1,47 @@
-package com.Project.BackEnd.BookMark.Service;public class BookMarkService {
+package com.Project.BackEnd.BookMark.Service;
+
+import com.Project.BackEnd.Board.Entity.Board;
+import com.Project.BackEnd.BookMark.Entity.BookMark;
+import com.Project.BackEnd.BookMark.Repository.BookMarkRepositoryByBoard;
+import com.Project.BackEnd.BookMark.Repository.BookMarkRepositoryByMember;
+import com.Project.BackEnd.DataNotFoundException;
+import com.Project.BackEnd.Member.Entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class BookMarkService {
+    private final BookMarkRepositoryByMember bookMarkRepositoryByMember;
+    private final BookMarkRepositoryByBoard bookMarkRepositoryByBoard;
+
+    public Board getBookMarkRepositoryByBoard(String title) {
+        Optional<Board> board = this.bookMarkRepositoryByBoard.findByTitle(title);
+        if (board.isPresent()) {
+            return board.get();
+        }
+        else {
+            throw new DataNotFoundException("Data not Found");
+        }
+    }
+
+    public Board getBookMarkRepositoryByMember(String name) {
+        Optional<Board> board = this.bookMarkRepositoryByMember.findByName(name);
+        if (board.isPresent()) {
+            return board.get();
+        }
+        else {
+            throw new DataNotFoundException("Data not Found");
+        }
+    }
+
+    public BookMark create(Member member, Board board) {
+        BookMark bookMark = new BookMark();
+        bookMark.setMember(member);
+        bookMark.setBoard(board);
+
+        return bookMark;
+    }
 }
