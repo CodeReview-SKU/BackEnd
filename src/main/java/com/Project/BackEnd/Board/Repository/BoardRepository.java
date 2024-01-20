58bcf5f5c9d20948c8ca344392b271763d052443
HwanheeMoon <yuiop0218@naver.com>
2024-01-20 20:36:17 +0900

    1.20 보드 CRUD 작성 및 테스트

--- src/main/java/com/Project/BackEnd/Board/Repository/BoardRepository.java
+++ src/main/java/com/Project/BackEnd/Board/Repository/BoardRepository.java
@@ -6,12 +6,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import com.Project.BackEnd.Board.Entity.Board.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Board findByTitle(String title); //제목으로 검색
-    Board findByTitleAndContent(String title, String content); //제목과 내용으로 검색
-    Board findByTitleLike(String title); //제목에 포함된 단어로 검색
+    List<Board> findByTitle(String title); //제목으로 검색
+
+    List<Board> findByTag(tag tag);
+
+    Page<Board> findByCategory(Pageable pageable, category category);
+
+    Page<Board> findByTag(Pageable pageable, tag tag);
 
     Page<Board> findAll(Pageable pageable); //모든 게시물 검색 (페이지 로드)
 }
--- src/main/java/com/Project/BackEnd/Board/Service/BoardService.java
+++ src/main/java/com/Project/BackEnd/Board/Service/BoardService.java
@@ -24,7 +24,7 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
 
-    public Board getBoard(Long id) {
+    public Board getBoard(Long id) {  // Read
         Optional<Board> board = this.boardRepository.findById(id);
         if (board.isPresent()) {
             return board.get();
@@ -35,8 +35,40 @@ public class BoardService {
 
     }
 
+    public List<Board> getBoardByTitle(String title) {
+        List<Board> board = this.boardRepository.findByTitle(title);
+        if (!board.isEmpty()) {
+            return board;
+        }
+        else {
+            throw new DataNotFoundException("Data not found");
+        }
+    }
+
+    public List<Board> getBoardByTitleLike(String title) { // 게시물에 포함된 문자열에 대해서 리스트 반환
+        List<Board> board = this.boardRepository.findAll();
+        board.removeIf(t -> !t.getTitle().contains(title));
+        if (! board.isEmpty()) {
+            return board;
+        }
+        else {
+            throw new DataNotFoundException("Data not found");
+        }
+    }
+
+    public List<Board> getBoardByTag(tag tag) {
+        List<Board> boards = this.boardRepository.findByTag(tag);
+        if(!boards.isEmpty()){
+            return boards;
+        }
+        else {
+            throw new DataNotFoundException("Data not found");
+        }
+
+    }
+
     // 멤버, 제목, 내용, 카테고리, 북마크(0), 태그
-    public void create(Member member, String title, String content,
+    public void create(Member member, String title, String content, // Create
                        category category, tag tag) {
         Board board = new Board();
         board.setMember(member);
@@ -53,6 +85,18 @@ public class BoardService {
         this.boardRepository.save(board);
     }
 
+    public void delete(Board board) { // Delete
+        this.boardRepository.delete(board);
+    }
+
+    public void update(Board board, String title, String content, category category, tag tag) { //Update
+        board.setTitle(title);
+        board.setContent(content);
+        board.setCategory(category);
+        board.setTag(tag);
+        this.boardRepository.save(board);
+    }
+
     public void addImage(Board board, String image) { // 이미지 추가 메소드
         board.setImage(image);
         this.boardRepository.save(board);
@@ -64,10 +108,24 @@ public class BoardService {
     }
 
 
-    public Page<Board> getList(int page) {  // 페이지 20개의 게시물 리스트로 보여주는 부분
+    public Page<Board> getList(int page) {  // 페이지 8개의 게시물 리스트로 보여주는 부분
         List<Sort.Order> list = new ArrayList<>();
         list.add(Sort.Order.desc("write_date")); // 작성 일시를 기준으로 내림차순
-        Pageable pageable = PageRequest.of(page, 20, Sort.by(list));
+        Pageable pageable = PageRequest.of(page, 8, Sort.by(list));
         return this.boardRepository.findAll(pageable);
+    }
+
+    public Page<Board> getListByCategory(int page, category category) { // 카테고리 별로 탐색
+        List<Sort.Order> list = new ArrayList<>();
+        list.add(Sort.Order.desc("write_date"));
+        Pageable pageable = PageRequest.of(page, 8, Sort.by(list));
+        return this.boardRepository.findByCategory(pageable, category);
+    }
+
+    public Page<Board> getListByTag(int page, tag tag) { // 태그 별로 탐색
+        List<Sort.Order> list = new ArrayList<>();
+        list.add(Sort.Order.desc("write_date"));
+        Pageable pageable = PageRequest.of(page, 8, Sort.by(list));
+        return this.boardRepository.findByTag(pageable, tag);
     }
 }
