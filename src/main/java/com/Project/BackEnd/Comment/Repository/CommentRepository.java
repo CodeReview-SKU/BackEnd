e982d66647219bfce0c18eee4745dba8d9704f44
eastwest9 <dong092276@gmail.com>
2024-01-22 18:01:54 +0900

    댓글-서비스01.22

---
+++ src/main/java/com/Project/BackEnd/Comment/Repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.Project.BackEnd.Comment.Repository;
+
+import com.Project.BackEnd.Comment.Entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByBoardId(Long boardId);
+
+    List<Comment> findByMemberId(Long memberId);
+}
