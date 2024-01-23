fcaaae0f0d17ee9b5b0192ebb2ccb083084f1475
HwanheeMoon <yuiop0218@naver.com>
2024-01-23 19:00:51 +0900

    BookMark:CRUD, Entity

--- src/main/java/com/Project/BackEnd/BookMark/Controller/BookMarkController.java
+++ src/main/java/com/Project/BackEnd/BookMark/Controller/BookMarkController.java
@@ -1,2 +1,51 @@
-package com.Project.BackEnd.BookMark.Controller;public class BookMarkController {
+package com.Project.BackEnd.BookMark.Controller;
+
+
+import com.Project.BackEnd.Board.Service.BoardService;
+import com.Project.BackEnd.BookMark.DTO.BookMarkDTO;
+import com.Project.BackEnd.BookMark.Entity.BookMark;
+import com.Project.BackEnd.BookMark.Service.BookMarkService;
+import com.Project.BackEnd.Member.Service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/book_mark")
+@RequiredArgsConstructor
+public class BookMarkController {
+    private final BookMarkService bookMarkService;
+    private final MemberService memberService;
+    private final BoardService boardService;
+
+
+    @GetMapping("/list/{name}")
+    public ResponseEntity<List<BookMark>> bookMarkDetail(@PathVariable String name) {
+        List<BookMark> bookMarks = this.bookMarkService.getBookMarkByMember(name);
+        return ResponseEntity.ok(bookMarks);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<List<BookMark>> delete(@PathVariable long id) {
+
+        this.bookMarkService.delete(this.bookMarkService.getBookMark(id));
+
+        return ResponseEntity.ok(this.bookMarkService.getBookMarkList());
+    }
+
+    @ResponseBody
+    @PostMapping("/new")
+    public ResponseEntity<List<BookMark>> create(@RequestBody BookMarkDTO bookMarkDTO) {
+        this.bookMarkService.create(bookMarkDTO.getMember(), bookMarkDTO.getBoard());
+
+        return ResponseEntity.ok(this.bookMarkService.getBookMarkList());
+    }
+
+
+
 }
--- src/main/java/com/Project/BackEnd/BookMark/Repository/BookMarkRepository.java
+++ src/main/java/com/Project/BackEnd/BookMark/Repository/BookMarkRepository.java
@@ -1,16 +1,20 @@
 package com.Project.BackEnd.BookMark.Repository;
 
+import com.Project.BackEnd.Board.Entity.Board;
 import com.Project.BackEnd.BookMark.Entity.BookMark;
+import com.Project.BackEnd.Member.Entity.Member;
+import com.Project.BackEnd.Member.Repository.MemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.awt.print.Book;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
-    Optional<BookMark> findByTitle(String title);
-    Optional<BookMark> findByName(String name);
+    List<BookMark> findByMember(Optional<Member> member);
+    List<BookMark> findByBoard(Board board);
 }
 
 
